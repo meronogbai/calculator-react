24e8b8a37f78575be2954e7b88cea32ad3d92e2a
Meron Ogbai <okbaymeron@gmail.com>
2021-02-03 22:59:28 +0300

    Use swith cases instead of if conditionals

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -4,20 +4,27 @@ export default function operate(numberOne, numberTwo, operation) {
   const x = new Big(numberOne);
   const y = new Big(numberTwo);
   let result;
-  if (operation === '-') {
-    result = x.minus(y).toString();
-  } else if (operation === '+') {
-    result = x.plus(y).toString();
-  } else if (operation === 'x') {
-    result = x.times(y).toString();
-  } else if (operation === '÷') {
-    result = x.div(y).toString();
-  } else if (operation === '%') {
-    result = `${y.div(100)}%`;
-  } else if (operation === '+/-') {
-    result = x.times(y).times(-1).toString();
-  } else {
-    return null;
+  switch (operation) {
+    case '-':
+      result = x.minus(y).toString();
+      break;
+    case '+':
+      result = x.plus(y).toString();
+      break;
+    case 'x':
+      result = x.times(y).toString();
+      break;
+    case '÷':
+      result = x.div(y).toString();
+      break;
+    case '%':
+      result = `${y.div(100)}%`;
+      break;
+    case '+/-':
+      result = x.times(y).times(-1).toString();
+      break;
+    default:
+      break;
   }
   return result;
 }
