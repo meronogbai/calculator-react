c39ce263d360bf7b4b769808b02185ed01ac1c69
Meron Ogbai <okbaymeron@gmail.com>
2021-02-05 11:15:11 +0300

    Add number buttons in calculate function

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -3,22 +3,49 @@ import operate from './operate';
 export default function calculate(calcData, buttonName) {
   let { total, next, operation } = calcData;
   switch (buttonName) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      if (next) {
+        next += buttonName;
+      } else {
+        next = buttonName;
+      }
+      break;
     case '+':
     case '-':
     case 'X':
     case '÷':
     case '%':
-      total = operate(total, next, operation);
-      next = null;
+      if (!operation) {
+        operation = buttonName;
+      }
+      if (total) {
+        total = operate(total, next, operation);
+      } else {
+        total = next;
+      }
       operation = buttonName;
+      next = null;
       break;
     case '=':
-      total = operate(total, next, operation);
-      next = null;
+      if (total) {
+        total = operate(total, next, operation);
+      } else {
+        total = next;
+      }
       operation = null;
+      next = null;
       break;
     case '+/-':
-      next *= -1;
+      next = (Number(next) * -1).toString();
       break;
     case '.':
       if (!next.includes('.')) {
