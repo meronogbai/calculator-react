114d9da0ed2c3f309a5aed24ef64f592ede36ade
Meron Ogbai <okbaymeron@gmail.com>
2021-02-05 07:44:47 +0300

    Make app component stateful

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
+/* eslint-disable react/no-unused-state */
 import '../App.css';
+import { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operatiion: null,
+    };
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
+}
