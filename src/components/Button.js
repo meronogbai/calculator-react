a85d2cdd8e03bb72b592b8ca831ec8c0337f755b
Meron Ogbai <okbaymeron@gmail.com>
2021-02-08 10:38:25 +0300

    Set classes for buttons programatically

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,22 @@ import PropTypes from 'prop-types';
 
 export default function Button({ name, clickHandler }) {
   const handleClick = () => clickHandler(name);
-
+  const getClass = name => {
+    switch (name) {
+      case '0':
+        return 'Button zero';
+      case '+':
+      case '-':
+      case 'X':
+      case '÷':
+      case '=':
+        return 'Button operator';
+      default:
+        return 'Button';
+    }
+  };
   return (
-    <button type="button" onClick={handleClick} className={name === '0' ? 'Button zero' : 'Button'}>
+    <button type="button" onClick={handleClick} className={getClass(name)}>
       {name}
     </button>
   );
