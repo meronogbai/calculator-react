7c7b0cc26fca53fa2fb91ae6a50c5d6d1b4a4fcd
Meron Ogbai <okbaymeron@gmail.com>
2021-02-08 11:12:31 +0300

    Add home and quote components

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+const Home = () => (
+  <div>
+    <h2>Welcome to our Website!</h2>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error,
+    sed? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+    inventore ducimus quae delectus molestias repudiandae fugiat repellendus atque! Cum, numquam.
+  </div>
+);
+
+export default Home;
